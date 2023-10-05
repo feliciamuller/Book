17c523a3de736890c79059c9f7fd1a9f531ddc10
Felicia Müller <felicia-muller@hotmail.com>
2023-10-05 12:19:39 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Book
+    {
+        //creating fields
+        public string _title;
+        public string _author;
+        public string _summary;
+
+        //creating constructor and initialize the fields
+        public Book(string title, string author, string summary)
+        {
+            _title = title;
+            _author = author;
+            _summary = summary;
+
+        }
+
+        //create method to print out info
+        public void PrintBookInfo()
+        {
+            Console.WriteLine(_title);
+            Console.WriteLine(_author);
+            Console.WriteLine(_summary);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv titeln på boken: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Skriv författaren av boken: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Skriv en summering av boken: ");
+            string summary = Console.ReadLine();
+
+            //create objekt of the class Book 
+            Book myBook = new Book(title, author, summary);
+
+            //calls method
+            myBook.PrintBookInfo();
+
+        }
+    }
+}
